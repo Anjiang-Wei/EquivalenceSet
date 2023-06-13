b532f1214cecf0876fae4403abac8adb6df45bef
Anjiang-Wei <1020681930@qq.com>
2023-06-12 23:20:27 -0700

    simplest algorithm works

--- algorithm.py
+++ algorithm.py
@@ -56,6 +56,13 @@ class Record(object):
                 field_parent[newkey] = grp_parent[key]
         return field_trace, field_parent
     def eval_algo(self, algo):
+        for key in self.field_trace:
+            trace_pointset_list_all = self.field_trace[key]
+            parent_point_set = self.field_parent[key]
+            for trace_pointset_list in trace_pointset_list_all:
+                bvh = algo.generate_bvh(trace_pointset_list, parent_point_set)
+                print(key, bvh)
+            algo.clear()
         pprint.pprint(self.field_trace)
         pprint.pprint(self.field_parent)
         # for key in self.grp_trace.keys():
@@ -69,15 +76,17 @@ class Algo(object):
     def union_point_set(point_set_list):
         union = set()
         for point_set in point_set_list:
-            for point in point_set:
+            for point in point_set.points:
                 union.add(point)
         return union
     def generate_bvh(self, trace_points_list, parent_point_set):
         # trace_points_list: the list of smallest points that each point task access
         # parent_point_set: all points that the index launch accesses
         if len(self.history_bvh) == 0:
-            if Algo.union_point_set(trace_points_list) == parent_point_set:
+            if Algo.union_point_set(trace_points_list) == parent_point_set.points:
                 self.history_bvh.append(trace_points_list)
             else:
                 assert False
         return self.history_bvh[0]
+    def clear(self):
+        self.history_bvh = []
